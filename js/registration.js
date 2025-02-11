5c2d36b160027767bac557d7d8151ea8517d8c57
arafatjim <jimyeasirarafat28@gmail.com>
2025-02-12 02:44:54 +0600

    1st commit

---
+++ js/registration.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-sign-up').addEventListener('click',function(event){
+          console.log('clicked sign up')
+          event.preventDefault();
+          var createPass=document.getElementById('create-pass').value;
+          var confirmPass=document.getElementById('confirm-pass').value;
+          var emailNumber=document.getElementById('emailNum').value;
+          
+          if(createPass== confirmPass){
+                    window.location.href='/home.html';
+                    emailNumber='';
+                    
+          }
+          
+          console.log(createPass);
+          console.log(confirmPass);
+})
